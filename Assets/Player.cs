51f289da6e6a0fbc56e39579443f6f2da752af1b
AlwenMoonbria <nixnaf@outlook.com>
2025-06-11 16:45:16 +0800

    移动与跳跃

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.XR;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    
+    
+    [SerializeField] float xInput;
+    [SerializeField] private float moveForce;
+    [SerializeField] private float jumpForce;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        xInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(xInput * moveForce, rb.velocity.y);
         
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x,jumpForce);
+        }
     }
 }
